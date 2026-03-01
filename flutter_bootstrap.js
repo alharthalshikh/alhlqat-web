194db744412be9b8b3b6edcc4f5700926699f0c1
alharth_allshikh <alharth465117@gmail.com>
2026-03-02 01:42:55 +0300

    Increase service worker timeout for mobile support

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,7 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "671017312"
+    serviceWorkerVersion: "671017312",
+    timeoutMillis: 30000
   }
 });
